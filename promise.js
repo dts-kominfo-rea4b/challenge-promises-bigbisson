55e71dc8e3275f97821c2a823f94f7b6c4b6bdba
bigbisson <bigmaster.indra@gmail.com>
2022-09-18 10:14:24 +0700

    fungsi promiseOutput dan ignore jsonconfig

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = emosi => new Promise(async (myResolve) => {
+
+  const allCinema = [].concat(await promiseTheaterIXX(), await promiseTheaterVGC());
+  let totalEmosi =  0;
+
+  try {
+    allCinema.forEach(data => {
+      totalEmosi += data.hasil === emosi ? 1 : 0;
+    });
+    
+    myResolve(totalEmosi);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = {
   promiseOutput,
